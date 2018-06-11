f6bf5675aa8ad76d7748dd9999346e4a56317c7c
Neil Gewirtz <ngewirtz86@gmail.com>
2018-06-11 16:36:49 -0400

    remove unused line

--- util/ajax.js
+++ util/ajax.js
@@ -1,5 +1,4 @@
 export const getForecast = () => {
-  const weatherRequest = new XMLHttpRequest();
   const accessId = "IXNIhCPJBQ0BoWNenOqFc";
   const secretKey = "lGrPYgi0EFwYUA9202hSFGxxRsOBOr4GJKlnh6hf";
   const url = `http://api.aerisapi.com/forecasts/11101?client_id=${accessId}&client_secret=${secretKey}`;
